959781cfbfb6cda67420c4566ba96d7da1965a66
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-08-31 22:35:23 +0100

    ➕🔔 add notification plugin

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -1,17 +1,19 @@
-<link rel="stylesheet" href="{{ asset('assets/css/simplebar.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/perfect-scrollbar.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/metisMenu.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/pace.min.css') }}">
+ <!--plugins-->
+ <link rel="stylesheet" href="{{ asset('assets/plugins/notifications/css/lobibox.min.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/plugins/metismenu/css/metisMenu.min.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/css/pace.min.css') }}">
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/icons.css') }}">
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/css/icons.css') }}">
 
-<!-- Theme Style CSS -->
-<link rel="stylesheet" href="{{ asset('assets/css/dark-theme.css') }}">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('assets/css/semi-dark.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/header-colors.css') }}">
+ <!-- Theme Style CSS -->
+ <link rel="stylesheet" href="{{ asset('assets/css/dark-theme.css') }}">
+ <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="{{ asset('assets/css/semi-dark.css') }}">
+ <link rel="stylesheet" href="{{ asset('assets/css/header-colors.css') }}">
 
-<link rel="stylesheet" href="{{ mix('assets/css/custom.css') }}">
+ <link rel="stylesheet" href="{{ mix('assets/css/custom.css') }}">

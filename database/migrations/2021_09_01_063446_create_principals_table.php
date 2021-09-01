e1f60eb7b0b9c3d2e4d121564873d6ae8a070ab3
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-09-01 08:50:24 +0100

    👨🏽‍💼 create Principal Model & migrations

---
+++ database/migrations/2021_09_01_063446_create_principals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrincipalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('principals', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('phone_number');
+            $table->rememberToken();
+            $table->string('profile_photo', 2048)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('principals');
+    }
+}
---
+++ database/seeders/PrincipalSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Principal;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class PrincipalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      Principal::create([
+        'firstname' => 'Sam',
+        'middlename' => 'Daniels',
+        'lastname' => 'Adekoya',
+        'email' => 'principal@gmail.com',
+        'phone_number' => '0802222222',
+        'password' => Hash::make('password'),
+    ]);
+    }
+}

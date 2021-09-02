b29eb5a5b07618172612da3a3fac46e2f4dd5164
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-09-02 06:53:38 +0100

    👨🏽‍💼 create teacher migration files

---
+++ database/migrations/2021_09_01_183917_create_teachers_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('firstname');
+            $table->string('middlename')->nullable();
+            $table->string('lastname');
+            $table->string('title');
+            $table->string('gender')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('class_teacher')->nullable();
+            $table->string('staff_id');
+            $table->string('email')->unique()->nullable();
+            $table->string('password');
+            $table->string('phone_number')->nullable();
+            $table->rememberToken();
+            $table->string('profile_photo', 2048)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}

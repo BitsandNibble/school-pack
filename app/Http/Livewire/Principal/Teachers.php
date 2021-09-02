6c2d6a73cb1f522ea84ab1c00d11943ccb4d9611
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-09-03 00:20:46 +0100

    ➕👨🏽‍💼 add teacher functionality done

--- app/Http/Livewire/Principal/Teachers.php
+++ app/Http/Livewire/Principal/Teachers.php
@@ -6,23 +6,24 @@ use App\Models\Teacher;
 use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 use Illuminate\Support\Str;
+use Livewire\WithPagination;
 
 class Teachers extends Component
 {
-  public $teacher, $teachers;
+  use WithPagination;
+  public $teacher;
 
   protected $rules = [
     'teacher.firstname' => 'required|string',
-    'teacher.middlename' => 'string',
     'teacher.lastname' => 'required|string',
     'teacher.title' => 'required',
   ];
 
   public function render()
   {
-    $this->teachers = Teacher::get();
+    $teachers = Teacher::Paginate(10);
 
-    return view('livewire.principal.teachers');
+    return view('livewire.principal.teachers', compact('teachers'));
   }
 
   public function saveTeacher()
@@ -31,18 +32,24 @@ class Teachers extends Component
 
     Teacher::create([
       'firstname' => $this->teacher['firstname'],
-      'middlename' => $this->teacher['middlename'],
+      'middlename' => $this->teacher['middlename'] ?? '',
       'lastname' => $this->teacher['lastname'],
       'title' => $this->teacher['title'],
-      'gender' => $this->teacher['gender'],
-      'class_teacher' => $this->teacher['class_teacher'],
+      'gender' => $this->teacher['gender'] ?? '',
+      'class_teacher' => $this->teacher['class_teacher'] ?? '',
       'staff_id' => 'GS_' . mt_rand(500, 1000),
       'password' => Hash::make('password'),
-      'slug' => Str::slug([$this->teacher['firstname'], $this->teacher['middlename'], $this->teacher['lastname']], '-'),
+      'slug' => Str::slug($this->teacher['firstname'], '-'),
     ]);
 
     session()->flash('message', 'Teacher Added Successfully');
+    $this->reset(['teacher']);
+    $this->emit('closeModal');
+  }
 
-    $this->emit('stored');
+  public function close()
+  {
+    $this->reset(['teacher']);
+    $this->emit('closeModal');
   }
 }
--- resources/views/livewire/principal/teachers.blade.php
+++ resources/views/livewire/principal/teachers.blade.php
@@ -1,80 +1,109 @@
-<x-card>
-  <x-button data-bs-toggle="modal" data-bs-target="#teacherModal">Add New Teacher</x-button>
-  {{-- <x-button wire:click="confirmTeacherAdd">Add New Teacher</x-button> --}}
-</x-card>
+<div>
+  <x-flash />
 
-<x-card>
-  @foreach ($teachers as $teacher)
-    {{ $teacher }}
-  @endforeach
-</x-card>
+  <x-card>
+    <x-button data-bs-toggle="modal" data-bs-target="#teacherModal">Add New Teacher</x-button>
+  </x-card>
 
+  <x-card>
+    <div class="table-responsive">
+      <table id="teachersTable" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+          <tr>
+            <th>S/N</th>
+            <th>Title</th>
+            <th>Full name</th>
+            <th>Staff ID</th>
+            <th>Email</th>
+            <th>Number</th>
+            <th>Class Teacher</th>
+          </tr>
+        </thead>
 
-@push('modals')
+        <tbody>
+          @foreach ($teachers as $teacher)
+            <tr>
+              <td>{{ $loop->iteration }}</td>
+              <td>{{ $teacher->title }}</td>
+              <td>{{ $teacher->firstname }} {{ $teacher->middlename }} {{ $teacher->lastname }}</td>
+              <td>{{ $teacher->staff_id }}</td>
+              <td>{{ $teacher->email }}</td>
+              <td>{{ $teacher->phone_number }}</td>
+              <td>{{ $teacher->class_teacher }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+
+      {{-- {{ $teachers->links() }} --}}
+    </div>
+
+  </x-card>
+
   <x-modal id="teacherModal">
     <x-slot name="title">Add New Teacher</x-slot>
 
     <x-slot name="content">
-      <p><span class="text-danger">*</span> fields are required</p>
-      <div class="row">
-        <x-validation-errors />
-        <div class="col-md-4">
-          <x-label for="firstname">First name <span class="text-danger">*</span></x-label>
-          <x-input type="text" id="firstname" wire:model.defer="teacher.firstname" />
-          <x-input-error for="firstname" />
+      <form>
+        <p><span class="text-danger">*</span> fields are required</p>
+
+        <div class="row">
+          {{-- <x-validation-errors /> --}}
+
+          <div class="col-md-4">
+            <x-label for="firstname">First name <span class="text-danger">*</span></x-label>
+            <x-input type="text" id="firstname" wire:model.defer="teacher.firstname" />
+            <x-input-error for="teacher.firstname" custom-message="The firstname is required" />
+          </div>
+
+          <div class="col-md-4">
+            <x-label for="middlename">Middle name</x-label>
+            <x-input type="text" id="middlename" wire:model.defer="teacher.middlename" />
+          </div>
+
+          <div class="col-md-4">
+            <x-label for="lastname">Last name <span class="text-danger">*</span></x-label>
+            <x-input type="text" id="lastname" wire:model.defer="teacher.lastname" />
+            <x-input-error for="teacher.lastname" custom-message="The lastname is required" />
+          </div>
         </div>
 
-        <div class="col-md-4">
-          <x-label for="middlename">Middle name</x-label>
-          <x-input type="text" id="middlename" wire:model.defer="teacher.middlename" />
-          <x-input-error for="middlename" />
-        </div>
+        <div class="row mt-2">
+          <div class="col-md-4">
+            <x-label for="title">Title <span class="text-danger">*</span></x-label>
+            <x-select id="title" wire:model.defer="teacher.title">
+              <option value="Mr">Mr</option>
+              <option value="Mrs">Mrs</option>
+              <option value="Ms">Ms</option>
+              <option value="Miss">Miss</option>
+            </x-select>
+            <x-input-error for="teacher.title" custom-message="The title is required" />
+          </div>
 
-        <div class="col-md-4">
-          <x-label for="lastname">Last name <span class="text-danger">*</span></x-label>
-          <x-input type="text" id="lastname" wire:model.defer="teacher.lastname" />
-          <x-input-error for="lastname" />
-        </div>
-      </div>
+          <div class="col-md-4">
+            <x-label for="gender">Gender</x-label>
+            <x-select id="gender" wire:model.defer="teacher.gender">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </x-select>
+          </div>
 
-      <div class="row mt-2">
-        <div class="col-md-4">
-          <x-label for="title">Title <span class="text-danger">*</span></x-label>
-          <x-select id="title" wire:model.defer="teacher.title">
-            <option value="Mr">Mr</option>
-            <option value="Mrs">Mrs</option>
-            <option value="Ms">Ms</option>
-            <option value="Miss">Miss</option>
-          </x-select>
+          <div class="col-md-4">
+            <x-label for="class_teacher">Class Teacher</x-label>
+            <x-select id="class_teacher" wire:model.defer="teacher.class_teacher">
+              <option value="JSS1">JSS1</option>
+              <option value="JSS2">JSS2</option>
+              <option value="JSS3">JSS3</option>
+            </x-select>
+          </div>
         </div>
-
-        <div class="col-md-4">
-          <x-label for="gender">Gender</x-label>
-          <x-select id="gender" wire:model.defer="teacher.gender">
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Other">Other</option>
-          </x-select>
-        </div>
-
-        <div class="col-md-4">
-          <x-label for="class_teacher">Class Teacher</x-label>
-          <x-select id="class_teacher" wire:model.defer="teacher.class_teacher">
-            <option value="JSS1">JSS1</option>
-            <option value="JSS2">JSS2</option>
-            <option value="JSS3">JSS3</option>
-          </x-select>
-        </div>
-
-        {{-- <div class="col-md-3">
-              <x-label for="staff_id">Staff ID</x-label>
-              <x-input disabled />
-            </div> --}}
-      </div>
-    </div>
+      </form>
+    </x-slot>
 
     <x-slot name="footer">
-      <x-button value="submit" wire:click="saveTeacher">Add</x-button>
+      <x-button value="dark" wire:click="close">Close</x-button>
+      <x-button value="submit" wire:click.prevent="saveTeacher">Save</x-button>
     </x-slot>
   </x-modal>
-@endpush
+</div>

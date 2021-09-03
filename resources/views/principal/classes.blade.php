6e42f2783f2f1686dd84744d2ca8989d243b8ab4
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-09-03 21:44:59 +0100

    Revert "➕📚 add subjects template"
    This reverts commit 49ff1f68ddf4a527b8c566685069762250b986f6.

--- resources/views/principal/classes.blade.php
+++ resources/views/principal/classes.blade.php
@@ -12,9 +12,9 @@
           <x-card>
             <div class="d-flex align-items-center">
               <div>
-                <h6 class="mb-1 text-dark">Class Teacher</h6>
-                <p class="mb-1 text-primary">Mr Something</p>
-                <h4>JSS1</h4>
+                <p class="mb-0 text-dark">Class Teacher</p>
+                <p class="mb-0 text-primary">Mr Something</p>
+                <h5 class="mb-0">JSS1</h5>
               </div>
               <div class="dropdown ms-auto" style="position: relative;">
                 <div class="dropdown-toggle dropdown-toggle-nocaret cursor-pointer" data-bs-toggle="dropdown">

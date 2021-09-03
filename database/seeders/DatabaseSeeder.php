5d107008ed24ac16b349c45b2cd56201b00b866b
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-09-03 21:44:59 +0100

    Revert "📄 create custom login page"
    This reverts commit 06aac1293e9e0be260d42b0ad6c2749ee99fa286.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,12 +14,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
-        // User::create([
-        //   'name' => 'Sam Billy',
-        //   'email' => 'sam@gmail.com',
-        //   'email_verified_at' => now(),
-        //   'password' => Hash::make('password'),
-        // ]);
     }
 }

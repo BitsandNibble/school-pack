73c9b35854cb62dfc846823d1f57ff9360176b96
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-09-03 19:01:30 +0100

    📝 admin can edit class

--- app/Http/Livewire/Principal/Classes.php
+++ app/Http/Livewire/Principal/Classes.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Principal;
 
 use App\Models\ClassRoom;
 use App\Models\Teacher;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,47 +12,81 @@ class Classes extends Component
 {
   use WithPagination;
 
-  public $class;
+  public $existingTeacher, $class_id, $name, $teacher_id;
+  public $reset = ['name', 'teacher_id', 'class_id'];
   protected $paginationTheme = 'bootstrap';
 
-  protected $rules = [
-    'class.name' => 'required|string|unique:class_rooms,name',
-  ];
+  protected $rules;
+  protected function rules()
+  {
+    return [
+      'name' => ['required', 'string', Rule::unique('class_rooms')->ignore($this->class_id)],
+    ];
+  }
 
   public function render()
   {
-    $classes = ClassRoom::with('teachers')->Paginate(10);
+    $classes = ClassRoom::orderBy('name')->with('teachers')->Paginate(6);
     $teachers = Teacher::whereDoesntHave('classRooms')->get();
 
     return view('livewire.principal.classes', compact('classes', 'teachers'));
   }
 
-  public function close()
+  public function cancel()
   {
-    $this->reset(['class']);
     $this->emit('closeModal');
+    $this->reset($this->reset);
+    // $this->reset(['name', 'teacher_id', 'class_id']);
+  }
+
+  public function edit($id)
+  {
+    $class = ClassRoom::where('id', $id)->with('teachers')->first();
+    $this->class_id = $class['id'];
+
+    $this->name = $class->name;
+    foreach ($class->teachers()->get() as $classTeacher) {
+      $this->teacher_id = $classTeacher->id;
+      $this->existingTeacher = $classTeacher->title . ' ' . $classTeacher->fullname;
+    }
   }
 
   public function store()
   {
-    $this->validate();
+    $validated = $this->validate();
 
-    $class = ClassRoom::create([
-      'name' => $this->class['name'],
-      'teacher_id' => $this->class['teacher_id'] ?? null,
-    ]);
-
-    if (!empty($this->class['teacher_id'])) {
-      $class->teachers()->sync($this->class['teacher_id']);
+    if ($this->class_id) {
+      $class = ClassRoom::find($this->class_id);
+      $class->update($validated);
+      session()->flash('message', 'Class Updated Successfully');
+    } else {
+      $class = ClassRoom::create($validated);
+      session()->flash('message', 'Class Added Successfully');
     }
 
-    session()->flash('message', 'Class Added Successfully');
-    $this->reset(['class']);
+    if (!empty($this->teacher_id)) {
+      $class->teachers()->sync($this->teacher_id);
+    }
+
     $this->emit('closeModal');
+    $this->reset($this->reset);
+  }
+
+  public function deleteExistingTeacher($id)
+  {
+    $teacher = Teacher::where('id', $id)->with('classRooms')->first();
+
+    foreach ($teacher->classRooms()->get() as $teacherClass) {
+      $teacher->classRooms()->detach($teacherClass->id);
+    }
+
+    $this->emit('closeModal');
+    $this->reset($this->reset);
   }
 
   public function delete(ClassRoom $class)
   {
+    $class->teachers()->detach($this->teacher_id);
     $class->delete();
   }
 }

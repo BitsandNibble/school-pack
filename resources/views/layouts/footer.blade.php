25113606bf89f0bfe45385af8868b756479cf24e
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-09-03 21:44:59 +0100

    Revert "🐾 update footer"
    This reverts commit 05842bd3810cc504f7010d8a2b8effab75ca1477.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,5 +2,5 @@
 <a href="javaScript:;" class="back-to-top"><i class='bx bxs-up-arrow-alt'></i></a>
 <!--End Back To Top Button-->
 <footer class="page-footer">
-  <p class="mb-0">© 2021 {{ date('Y') == 2021 ? '' : '- ' . date('Y') }} Bits and Nibble, Inc.</p>
+  <p class="mb-0">Copyright © 2021 - {{ date('Y') }}. All right reserved.</p>
 </footer>

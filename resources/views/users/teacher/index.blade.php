c301f0f15e263e7efd34137a7dd75e958381d3a8
Favour Okunowo <toluwaniokunowo@gmail.com>
2021-09-10 23:48:00 +0100

    👨🏼‍🏫 teacher: show class on dashboard

--- resources/views/users/teacher/index.blade.php
+++ resources/views/users/teacher/index.blade.php
@@ -1,3 +1,32 @@
 <x-app-layout>
-  {{ auth()->user()->fullname }}
+  <hr>
+  @forelse(auth()->user()->classes as $class)
+    <div class="row">
+      <div class="col-4">
+        <x-card>
+          <div class="text-center">
+            <div class="widgets-icons rounded-circle mx-auto bg-light-success text-success mb-3"><i
+                  class="bx bxs-graduation"></i>
+            </div>
+            <h4 class="my-1">{{ $class->name }}</h4>
+            <p class="mb-0 text-secondary">Class teacher for this session</p>
+          </div>
+        </x-card>
+      </div>
+    </div>
+  @empty
+    <div class="row">
+      <div class="col-4">
+        <x-card>
+          <div class="text-center">
+            <div class="widgets-icons rounded-circle mx-auto bg-light-success text-success mb-3"><i
+                  class="bx bxs-graduation"></i>
+            </div>
+            <h4 class="my-1">None for this term</h4>
+            <p class="mb-0 text-secondary">Class teacher for this session</p>
+          </div>
+        </x-card>
+      </div>
+    </div>
+  @endforelse
 </x-app-layout>

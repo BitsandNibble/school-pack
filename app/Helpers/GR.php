615b05b640a35c9ed33092ee7f337ae1a358b579
Favour <toluwaniokunowo@gmail.com>
2021-09-25 16:06:49 +0100

    ➕ create new helper file

---
+++ app/Helpers/GR.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Helpers;
+
+class GR
+{
+  public static function get_remarks()
+  {
+    return [
+      'Average', 'Credit', 'Distinction', 'Excellent', 'Fail',
+      'Fair', 'Good', 'Pass', 'Poor', 'Very Good', 'Very Poor',
+    ];
+  }
+}

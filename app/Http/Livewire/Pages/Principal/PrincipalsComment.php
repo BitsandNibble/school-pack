fc840f2ec4ec889dc47dc8a752a5bd1d0121b7dc
Favour <toluwaniokunowo@gmail.com>
2021-10-07 08:53:32 +0100

    👥 clone teacher components to principal

---
+++ app/Http/Livewire/Pages/Principal/PrincipalsComment.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Principal;
+
+use App\Models\ExamRecord;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class PrincipalsComment extends Component
+{
+  public $teachers_comment;
+  public $student_id;
+
+  protected array $rules = [
+    'teachers_comment' => 'required|string',
+  ];
+
+  public function mount($id): void
+  {
+    $this->student_id = $id;
+  }
+
+  public function render(): Factory|View|Application
+  {
+    $this->teachers_comment = ExamRecord::where(['student_id' => $this->student_id])
+      ->first()
+      ->teachers_comment;
+
+    return view('livewire.pages.principal.principals-comment');
+  }
+
+  public function store(): void
+  {
+    $this->validate();
+
+    ExamRecord::where(['student_id' => $this->student_id])->update([
+      'teachers_comment' => $this->teachers_comment,
+    ]);
+
+    session()->flash('message', 'Comment Added Successfully');
+  }
+}
---
+++ resources/views/livewire/pages/principal/tabulation-sheet.blade.php
@@ -0,0 +1,52 @@
+<div>
+  <x-breadcrumb>
+    Results
+    <li class="breadcrumb-item active" aria-current="page">Tabulation Sheet</li>
+  </x-breadcrumb>
+
+  <x-card>
+    <div class="row">
+      <div class="col-md-4 mb-2">
+        <x-label for="exam">Exam</x-label>
+        <x-select id="exam" wire:model="exam_id">
+          @foreach($exams as $exam)
+            <option value="{{ $exam->id }}">{{ $exam->name }}</option>
+          @endforeach
+        </x-select>
+        <x-input-error for="exam_id" />
+      </div>
+
+      <div class="col-md-4 mb-2">
+        <x-label for="class">Class</x-label>
+        <x-select id="class" wire:model="class_id">
+          @if(count($classes) > 0)
+            @foreach($classes as $class)
+              <option value="{{ $class->class_room_id }}">{{ $class->class_room->name }}</option>
+            @endforeach
+          @endif
+        </x-select>
+        <x-input-error for="class_id" />
+      </div>
+
+{{--      <div class="col-md-4 mb-2">--}}
+{{--        <x-label for="subject">Subject</x-label>--}}
+{{--        <x-select id="subject" wire:model.defer="subject_id">--}}
+{{--          @if(count($subjects) > 0)--}}
+{{--            @foreach($subjects as $subject)--}}
+{{--              <option value="{{ $subject->subject->id }}">{{ $subject->subject->name }}</option>--}}
+{{--            @endforeach--}}
+{{--          @endif--}}
+{{--        </x-select>--}}
+{{--        <x-input-error for="subject_id" />--}}
+{{--      </div>--}}
+
+      <div class="d-grid gap-2">
+        <x-button wire:click.prevent="view">View Sheet</x-button>
+      </div>
+    </div>
+  </x-card>
+
+  <livewire:pages.teacher.tabulation />
+
+  <x-spinner />
+</div>

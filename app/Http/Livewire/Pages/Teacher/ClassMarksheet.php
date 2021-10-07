86d3079e0c0b391eb51e58432304a71d40af568e
Favour <toluwaniokunowo@gmail.com>
2021-10-07 04:01:58 +0100

    👁👨🏽‍🎓🏫 show students in assigned class of auth. teacher

---
+++ app/Http/Livewire/Pages/Teacher/ClassMarksheet.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Teacher;
+
+use App\Helpers\SP;
+use App\Models\ClassRoom;
+use App\Models\Exam;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Livewire\Component;
+
+class ClassMarksheet extends Component
+{
+  public $class_id;
+  public $section_id;
+  public $data;
+  public $students;
+
+  protected $listeners = ['getValues'];
+
+  public function render(): Factory|View|Application
+  {
+    if ($this->class_id){
+
+//      get students along with the subjects they're registered with to show in table body
+      $this->students = \App\Models\Student::where($this->data)
+        ->get();
+    }
+
+    return view('livewire.pages.teacher.class-marksheet');
+  }
+
+  public function getValues($value): void
+  {
+    $this->class_id = $value['class_id'];
+    $this->section_id = $value['section_id'];
+
+//    use this for where clause to avoid duplicates
+    $this->data = [
+      'class_room_id' => $this->class_id,
+      'section_id' => $this->section_id,
+    ];
+  }
+}
---
+++ resources/views/livewire/pages/teacher/class-marksheet.blade.php
@@ -0,0 +1,37 @@
+<div>
+  <x-card>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm" style="width:100%">
+        <thead>
+          <tr>
+            <th>S/N</th>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Adm. No</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          @if($students)
+            @forelse($students as $s)
+              <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td></td>
+                <td>{{ $s->fullname }}</td>
+                <td>{{ $s->school_id }}</td>
+                <td>
+                  <x-button-link value="danger" href="{{ route('select_year', [$s->id]) }}">View Marksheet</x-button-link>
+                </td>
+              </tr>
+            @empty
+              <tr>
+                <td colspan="5" class="text-center">No record found</td>
+              </tr>
+            @endforelse
+          @endif
+        </tbody>
+      </table>
+    </div>
+  </x-card>
+</div>

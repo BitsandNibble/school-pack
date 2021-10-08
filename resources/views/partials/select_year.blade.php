4a29b14718ff658625bdbf473ce750f0dcddd72e
Favour <toluwaniokunowo@gmail.com>
2021-10-08 10:28:28 +0100

    ➕🔐 add validation for selected year

--- resources/views/partials/select_year.blade.php
+++ resources/views/partials/select_year.blade.php
@@ -8,15 +8,15 @@
   <x-card>
     <div class="row d-flex justify-content-center text-center">
       <div class="col-6">
-        <form action="{{ route('teacher.result.marksheet.select_year') }}" method="POST">
+
+        <form action="{{ route('result.marksheet.show', [$student_id]) }}" method="POST">
           @csrf
           <x-label for="select_year" class="fw-bolder">Select Exam Year</x-label>
           <x-select id="select_year" class="mb-2" name="year">
             <option
                 value="{{ \App\Helpers\SP::getSetting('current_session') }}">{{ \App\Helpers\SP::getSetting('current_session') }}</option>
           </x-select>
-
-          <x-input type="hidden" value="{{ $student_id }}" name="id"></x-input>
+          <x-validation-errors />
 
           <x-button type="submit">Submit</x-button>
         </form>

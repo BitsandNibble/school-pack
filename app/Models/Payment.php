1164217687d4bc67fa3e147f7f60c563d15c9d2e
Favour <toluwaniokunowo@gmail.com>
2021-10-20 19:45:48 +0100

    ➕ add payment model & table

---
+++ app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Payment extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [
+    'title', 'amount', 'class_room_id',
+    'description', 'year', 'ref_no'
+  ];
+
+  public function class_room(): BelongsTo
+  {
+    return $this->belongsTo(ClassRoom::class);
+  }
+}

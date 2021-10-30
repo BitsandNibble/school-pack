21232ad6fa000fac1cfa4733486821b478407620
Favour <toluwaniokunowo@gmail.com>
2021-10-30 23:56:10 +0100

    🏋️‍♀️✍🏼 work on edit

--- app/Http/Livewire/Pages/Accountant/ManagePayment.php
+++ app/Http/Livewire/Pages/Accountant/ManagePayment.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Livewire\Pages\Accountant;
 
+use App\Helpers\SP;
+use App\Models\ClassRoom;
 use App\Models\Payment;
-use App\Models\Subject;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -13,24 +14,40 @@ class ManagePayment extends Component
 {
   public $exam_year;
   public $deleting;
-
-  protected $listeners = ['selected_year'];
+  public $payment_id;
+  public $payment;
 
   protected array $rules = [
-    'exam_year' => 'required'
+    'exam_year' => 'required',
+    'payment.title' => 'required|string',
+    'payment.class_room_id' => 'sometimes',
+    'payment.method' => 'sometimes',
+    'payment.amount' => 'required',
+    'payment.description' => 'sometimes|string',
   ];
+
+  protected array $validationAttributes = [
+    'payment.title' => 'title',
+    'payment.class_room_id' => 'class',
+    'payment.method' => 'method',
+    'payment.amount' => 'amount',
+    'payment.description' => 'description',
+  ];
+
+  protected $listeners = ['selected_year'];
 
   public function render(): Factory|View|Application
   {
     $payments = Payment::with('class_room')->get();
+    $classes = ClassRoom::get();
 
-    return view('livewire.pages.accountant.manage-payment', compact('payments'));
+    return view('livewire.pages.accountant.manage-payment', compact('payments', 'classes'));
   }
 
   public function cancel(): void
   {
     $this->emit('closeModal');
-    $this->reset('deleting');
+    $this->reset('deleting', 'payment_id');
   }
 
   public function selected_year($value): void
@@ -38,8 +55,30 @@ class ManagePayment extends Component
     $this->exam_year = $value['exam_year'];
   }
 
-  public function submit()
+  public function edit($id): void
   {
+    $this->payment = Payment::find($id);
+    $this->payment_id = $id;
+  }
+
+  public function store(): void
+  {
+    $this->validate();
+
+    if ($this->payment_id) {
+      Payment::find($this->payment_id)->update([
+        'title' => $this->payment['title'] ?? $this->payment->title,
+        'amount' => $this->payment['amount'] ?? $this->payment->amount,
+        'ref_no' => now()->year . '/' . random_int(100000, 999999),
+        'method' => $this->payment['method'] ?? $this->payment->method,
+        'class_room_id' => $this->payment['class'] !== 'NULL' ? $this->payment['class'] : NULL,
+        'description' => $this->payment['description'] ?? $this->payment->desctiption,
+        'year' => $this->payment->year ?? SP::getSetting('current_session'),
+      ]);
+
+      $this->cancel();
+      session()->flash('message', 'Payment Updated Successfully');
+    }
   }
 
   public function openDeleteModal($id): void
--- resources/views/livewire/pages/accountant/manage-payment.blade.php
+++ resources/views/livewire/pages/accountant/manage-payment.blade.php
@@ -32,7 +32,11 @@
                 <td>{{ $payment->class_room->name ?? '' }}</td>
                 <td>{{ $payment->method ?? '' }}</td>
                 <td>{{ $payment->description ?? '' }}</td>
-                <td></td>
+                <td>
+                  <x-button class="px-0" wire:click="edit({{ $payment->id }})" value="" data-bs-toggle="modal"
+                            data-bs-target="#paymentModal">
+                    <i class="bx bxs-pen"></i>
+                  </x-button>
                   <x-button class="px-0" value="" wire:click="openDeleteModal({{ $payment->id }})"
                             data-bs-toggle="modal" data-bs-target="#deleteModal">
                     <i class="bx bxs-trash-alt"></i>
@@ -48,6 +52,53 @@
         </table>
       </div>
     </x-card-with-header>
+
+    <x-confirmation-modal id="paymentModal">
+      <x-slot name="title">Edit Payment</x-slot>
+
+      <x-slot name="content">
+        <div class="col-md-6 mb-2">
+          <x-label>Title <span class="text-danger">*</span></x-label>
+          <x-input type="text" wire:model.defer="payment.title" placeholder="E.g School Fees" />
+          <x-input-error for="payment.title" />
+        </div>
+
+        <div class="col-md-6 mb-2">
+          <x-label>Class</x-label>
+          <x-select wire:model.defer="payment.class_room_id">
+            <option selected value="NULL">All Classes</option>
+            @foreach($classes as $class)
+              <option value="{{ $class->id }}">{{ $class->name }}</option>
+            @endforeach
+          </x-select>
+        </div>
+
+        <div class="col-md-6 mb-2">
+          <x-label>Payment Method</x-label>
+          <x-select wire:model.defer="payment.method">
+            <option value="cash" selected>Cash</option>
+            <option value="online" disabled>Online</option>
+          </x-select>
+        </div>
+
+        <div class="col-md-6 mb-2">
+          <x-label>Amount(N) <span class="text-danger">*</span></x-label>
+          <x-input type="text" wire:model.defer="payment.amount" />
+          <x-input-error for="payment.amount" />
+        </div>
+
+        <div class="col-md-6 mb-2">
+          <x-label>Description</x-label>
+          <x-textarea placeholder="" wire:model.defer="payment.description"></x-textarea>
+          <x-input-error for="payment.description" />
+        </div>
+      </x-slot>
+
+      <x-slot name="footer">
+        <x-button value="dark" wire:click="cancel">Close</x-button>
+        <x-button value="submit" wire:click.prevent="store">Update</x-button>
+      </x-slot>
+    </x-confirmation-modal>
 
     <x-confirmation-modal id="deleteModal">
       <x-slot name="title">Delete Payment</x-slot>

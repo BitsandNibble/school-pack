d11ccda401915c0d81347cdee61de41eca06c5be
Favour <toluwaniokunowo@gmail.com>
2021-10-31 07:41:24 +0100

    ➕🗃👨🏽‍🎓💰 add files for student payment

---
+++ app/Http/Livewire/Pages/Accountant/StudentPayment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Accountant;
+
+use Livewire\Component;
+
+class StudentPayment extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.accountant.student-payment');
+    }
+}
---
+++ app/Http/Livewire/Pages/Accountant/StudentPaymentYear.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Accountant;
+
+use Livewire\Component;
+
+class StudentPaymentYear extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.accountant.student-payment-year');
+    }
+}

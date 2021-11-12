22118d4c3dbb1d623f8a49e3f4a7d875233d198b
Favour <toluwaniokunowo@gmail.com>
2021-11-12 20:08:18 +0100

    ➕👬 add author relationship

--- app/Models/NoticeBoard.php
+++ app/Models/NoticeBoard.php
@@ -2,17 +2,28 @@
 
 namespace App\Models;
 
+use App\Traits\WithSearch;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @method static when($q, \Closure $param)
+ * @method static find(mixed $teacher_id)
+ * @method static create(array $array)
+ * @method static where(string $string, $id)
  */
 class NoticeBoard extends Model
 {
   use HasFactory;
+  use WithSearch;
 
   protected $fillable = [
     'message', 'author_id',
   ];
+
+  public function principal(): BelongsTo
+  {
+    return $this->belongsTo(Principal::class, 'author_id')->withDefault();
+  }
 }

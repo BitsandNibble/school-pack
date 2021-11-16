a309ef56a0323e46f37a37acccc1ed23e9c32a75
Favour <toluwaniokunowo@gmail.com>
2021-11-16 12:31:33 +0100

    ⬆️👁📅 update & show data based on year

--- app/Http/Livewire/Components/Skills.php
+++ app/Http/Livewire/Components/Skills.php
@@ -7,27 +7,39 @@ use App\Models\Skill;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use JsonException;
 use Livewire\Component;
 
 class Skills extends Component
 {
+  use LivewireAlert;
+
   public int $student_id;
+  public $year;
+  public $data;
   public array $af = [];
   public array $ps = [];
 
-  public function mount($id): void
+  public function mount($id, $year): void
   {
     $this->student_id = $id;
+    $this->year = $year;
+    $this->data = [
+      'student_id' => $this->student_id,
+      'year' => $this->year,
+    ];
   }
 
   /**
-   * @throws \JsonException
+   * @throws JsonException
    */
   public function render(): Factory|View|Application
   {
     $skills = Skill::get();
-    $afs = ExamRecord::where(['student_id' => $this->student_id])->first()->af;
-    $pss = ExamRecord::where(['student_id' => $this->student_id])->first()->ps;
+
+    $afs = ExamRecord::where($this->data)->first()->af;
+    $pss = ExamRecord::where($this->data)->first()->ps;
 
     $decode_afs = empty($afs) ? '' : json_decode($afs, true, 512, JSON_THROW_ON_ERROR);
     $decode_pss = empty($pss) ? '' : json_decode($pss, true, 512, JSON_THROW_ON_ERROR);
@@ -38,15 +50,20 @@ class Skills extends Component
     return view('livewire.components.skills', compact('skills'));
   }
 
+  /**
+   * @throws JsonException
+   */
   public function store(): void
   {
     $af_skill = json_encode($this->af, JSON_THROW_ON_ERROR | JSON_FORCE_OBJECT);
     $ps_skill = json_encode($this->ps, JSON_THROW_ON_ERROR | JSON_FORCE_OBJECT);
 
-    $exam_record = ExamRecord::where(['student_id' => $this->student_id])->first();
+    $exam_record = ExamRecord::where($this->data)->first();
     $exam_record->update([
       'af' => $af_skill,
       'ps' => $ps_skill,
     ]);
+
+    $this->alert('success', 'Skill Updated Successfully');
   }
 }

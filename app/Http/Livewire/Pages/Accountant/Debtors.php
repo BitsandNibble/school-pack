a312d3c431f1a2099db3fc6688ef674166720594
Favour <toluwaniokunowo@gmail.com>
2021-11-17 14:19:52 +0100

    👁💰👨🏽‍🎓 show debtors per year

--- app/Http/Livewire/Pages/Accountant/Debtors.php
+++ app/Http/Livewire/Pages/Accountant/Debtors.php
@@ -12,27 +12,45 @@ use Livewire\Component;
 
 class Debtors extends Component
 {
+  public $selected_year;
+  public $debtors;
   public $class_id;
+  public $student_ids;
+  public $students;
+  public $classes;
 
   public function render(): Factory|View|Application
   {
-    $debtors = PaymentRecord::where('paid', 0)
-      ->with('student', 'payment')->get();
+    $years = PaymentRecord::where('paid', 0)
+      ->select('year')->distinct()->get();
 
-    foreach ($debtors as $debtor) {
-      $this->class_id = $debtor->student->class_room_id;
+    if ($this->selected_year) {
+      $this->debtors = PaymentRecord::where('paid', 0)
+        ->where('year', $this->selected_year)
+        ->with('student', 'payment')->get();
+
+      foreach ($this->debtors as $debtor) {
+        $this->class_id = $debtor->student->class_room_id;
+      }
+
+      $this->student_ids = PaymentRecord::where('paid', 0)->distinct()
+        ->select('student_id')
+        ->get()->pluck('student_id');
+
+      $this->students = Student::whereIn('id', $this->student_ids)->distinct()
+        ->select('class_room_id')
+        ->get()->pluck('class_room_id');
+
+      $this->classes = ClassRoom::whereIn('id', $this->students)->get();
     }
 
-    $student_ids = PaymentRecord::where('paid', 0)->distinct()
-      ->select('student_id')
-      ->get()->pluck('student_id');
+    return view('livewire.pages.accountant.debtors', compact('years'));
+  }
 
-    $students = Student::whereIn('id', $student_ids)->distinct()
-      ->select('class_room_id')
-      ->get()->pluck('class_room_id');
-
-    $classes = ClassRoom::whereIn('id', $students)->get();
-
-    return view('livewire.pages.accountant.debtors', compact('debtors', 'classes'));
+  public function submit()
+  {
+    $value = $this->validate(
+      ['selected_year' => 'required']
+    );
   }
 }

b598c13712fd4e91544635b39e8906ae81cddf9d
Favour <toluwaniokunowo@gmail.com>
2021-11-18 15:31:04 +0100

    🚜 refactor ManagePayment into 1 component

--- resources/views/livewire/pages/accountant/manage-payment.blade.php
+++ resources/views/livewire/pages/accountant/manage-payment.blade.php
@@ -1,4 +1,27 @@
 <div>
+  <x-breadcrumb>
+    Payments
+    <li class="breadcrumb-item active" aria-current="page">Manage Payments</li>
+  </x-breadcrumb>
+
+  <x-card>
+    <div class="row d-flex justify-content-center text-center">
+      <div class="col-6">
+        <x-label for="select_year" class="fw-bolder">Select Session/Year</x-label>
+        <x-select id="select_year" class="mb-2" wire:model.defer="session_year">
+          @foreach($years as $year)
+            <option value="{{ $year->year }}" selected>{{ $year->year }}</option>
+          @endforeach
+        </x-select>
+        <x-input-error for="session_year" />
+
+        <x-button class="mt-2" wire:click.prevent="submit">Submit</x-button>
+      </div>
+    </div>
+  </x-card>
+
+  <x-spinner />
+
   @if($session_year)
     <x-card-with-header>
       <x-slot name="header">
@@ -108,8 +131,6 @@
         <x-button value="danger" wire:click.prevent="delete({{ $deleting }})">Delete</x-button>
       </x-slot>
     </x-confirmation-modal>
-
-    <x-spinner />
   @endif
 
   @push('scripts')
@@ -124,4 +145,5 @@
         });
     </script>
   @endpush
+
 </div>

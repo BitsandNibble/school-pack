59b2e9f4ce5a9b6f0f91a91f563555429930ae48
Favour <toluwaniokunowo@gmail.com>
2021-11-18 16:38:54 +0100

    🚜 refactor MarkSheet into 1 component

--- resources/views/livewire/components/mark-sheet.blade.php
+++ resources/views/livewire/components/mark-sheet.blade.php
@@ -9,9 +9,9 @@
       <div class="col-md-4 mb-2">
         <x-label for="class">Class</x-label>
         <x-select id="class" wire:model="class_id">
-            @foreach($classes as $class)
-              <option value="{{ $class->class_room->id }}">{{ $class->class_room->name }}</option>
-            @endforeach
+          @foreach($classes as $class)
+            <option value="{{ $class->class_room->id }}">{{ $class->class_room->name }}</option>
+          @endforeach
         </x-select>
         <x-input-error for="class_id" />
       </div>
@@ -34,7 +34,52 @@
     </div>
   </x-card>
 
-  <livewire:components.class-marksheet />
-
   <x-spinner />
+
+  @if($selected)
+    <x-card>
+      <x-responsive-table>
+        <thead>
+          <tr>
+            <th>S/N</th>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Adm. No</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          @forelse($students as $st)
+            <tr>
+              <td>{{ $loop->iteration }}</td>
+              <td></td>
+              <td>{{ $st->fullname }}</td>
+              <td>{{ $st->school_id }}</td>
+              <td>
+                <div class="dropdown">
+                  <x-button class="dropdown-toggle" value="danger" data-bs-toggle="dropdown" aria-expanded="false">
+                    View Marksheet
+                  </x-button>
+                  <ul class="dropdown-menu">
+                    @foreach($marks->where('student_id', $st->id)->pluck('year')->unique() as $year)
+                      <li><a class="dropdown-item" target="_blank"
+                             href="{{ route('result.marksheet.show', [$st->id, $year]) }}">
+                          {{ $year }}</a>
+                      </li>
+                    @endforeach
+                  </ul>
+                </div>
+              </td>
+            </tr>
+          @empty
+            <tr>
+              <td colspan="5" class="text-center">No record found</td>
+            </tr>
+          @endforelse
+        </tbody>
+      </x-responsive-table>
+
+    </x-card>
+  @endif
 </div>

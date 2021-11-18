79ee1ee7f01840758442cba1db5724aad4c46ee9
Favour <toluwaniokunowo@gmail.com>
2021-11-18 16:32:37 +0100

    🚜 refactor TabulationSheet into 1 component

--- app/Http/Livewire/Components/TabulationSheet.php
+++ app/Http/Livewire/Components/TabulationSheet.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire\Components;
 use App\Models\ClassRoom;
 use App\Models\ClassSubjectTeacher;
 use App\Models\Exam;
+use App\Models\ExamRecord;
+use App\Models\Mark;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -17,8 +19,15 @@ class TabulationSheet extends Component
   public $subject_id;
   public $classes = [];
 //  public $subjects = [];
+  public $data;
+  public $marks;
+  public $subjects;
+  public $students;
+  public $exam_record;
+  public $class_name;
+  public $exam_name;
+  public $selected_year;
 
-  protected $listeners = ['create_marks'];
   protected array $rules = [
     'exam_id' => 'required',
     'class_id' => 'required',
@@ -51,14 +60,45 @@ class TabulationSheet extends Component
       }
     }
 
+    if ($this->class_id) {
+//      get distinct subjects per class and show in table head
+      $this->subjects = Mark::where($this->data)
+        ->select('subject_id')
+        ->distinct()
+        ->with('subject')
+        ->get(['subject_id']);
+
+//      get students along with the subjects they're registered with to show in table body
+      $this->students = Mark::where($this->data)
+        ->select('student_id')
+        ->distinct()
+        ->with('student')
+        ->get(['student_id']);
+
+      $this->marks = Mark::where($this->data)
+        ->get(['student_id', 'subject_id', 'total_score']);
+
+      $this->exam_record = ExamRecord::where($this->data)
+        ->get(['student_id', 'total', 'average', 'position']);
+    }
+
     return view('livewire.components.tabulation-sheet', compact('exams'));
   }
-
 
 //  get values from select box
   public function view(): void
   {
-    $value = $this->validate();
-    $this->emit('getValues', $value);
+    $this->validate();
+
+//    use this for where clause to avoid duplicates
+    $this->data = [
+      'exam_id' => $this->exam_id,
+      'class_room_id' => $this->class_id,
+      'year' => $this->selected_year,
+    ];
+
+    $this->selected_year = Exam::where('id', $this->exam_id)->first()->session;
+    $this->class_name = ClassRoom::where('id', $this->class_id)->first()->name;
+    $this->exam_name = Exam::where('id', $this->exam_id)->first()->name;
   }
 }

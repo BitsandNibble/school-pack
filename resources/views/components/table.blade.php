9c32d16bbcff9ae40ed1d801a7292c69837692e9
Favour <toluwaniokunowo@gmail.com>
2022-04-04 04:21:59 +0100

    make table hover-able
    Signed-off-by: Favour <toluwaniokunowo@gmail.com>

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,6 +1,6 @@
 @props(['size' => 'sm'])
 
-<table {{ $attributes->merge(['class' => 'table table-striped table-'.$size], ['style' => 'width: 100%']) }}>
+<table {{ $attributes->merge(['class' => 'table table-hover table-striped table-'.$size], ['style' => 'width: 100%']) }}>
   <thead>
     <tr>
       {{ $head }}
